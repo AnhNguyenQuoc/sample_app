069686b3d60c2a122e8cc7d5414109519b728cbc
ANhNguyen <nguyenquocanh121296@gmail.com>
2018-01-22 07:56:46 +0000

    Add password reset

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-    attr_accessor   :remember_token, :activation_token
+    attr_accessor   :remember_token, :activation_token, :reset_token
     before_save     :downcase_email
-    before_create :create_activation_digest
+    before_create   :create_activation_digest
     
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :name, presence: true, length: {maximum: 50}
@@ -48,6 +48,20 @@ class User < ApplicationRecord
          UserMailer.account_activation(self).deliver_now
      end
      
+     #set the password reset attribute
+     def create_reset_password
+         self.reset_token = User.new_token
+         update_attribute(:reset_digest, User.digest(reset_token))
+         update_attribute(:reset_sent_at, Time.zone.now)
+     end
+     
+     def send_password_reset_email
+         UserMailer.password_reset(self).deliver_now
+     end
+     
+     def password_reset_expired?
+         reset_sent_at < 2.hours.ago
+     end
      private
      
      def downcase_email
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   
-  
+
   root 'static_pages#home'
   
   get '/home', to: 'static_pages#home'
@@ -17,5 +17,6 @@ Rails.application.routes.draw do
   
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets, only: [:new, :create, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
